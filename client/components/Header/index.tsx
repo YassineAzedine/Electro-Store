84c447969b76efda17bf5c65fd2f0f3160f7f1a9
yassine azedine <yassine.azedine0711@gmail.com>
2025-02-01 17:56:04 +0100

    simple modif

--- client/components/Header/index.tsx
+++ client/components/Header/index.tsx
@@ -13,7 +13,7 @@ const Header = () => {
       <Navbar bg="info" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <Link href="/" passHref>
-            <Navbar.Brand>Electro Store</Navbar.Brand>
+            <Navbar.Brand>Electro Storee</Navbar.Brand>
           </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
